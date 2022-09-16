51d8437c5023d04c87f4fb4bc38aa541ba31f399
Miguel Estrada <miguelestradam36@estudiantec.cr>
2022-09-15 19:55:22 -0600

    Starting application development

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
 import threading
+import yaml, os
 from modules.snowflake_connector_object.connector import SnowflakeConnector
+#-----------------------------------------------------------
+#-------------  Variables                          ---------
+#-----------------------------------------------------------
+
+fullpath = os.path.join(os.path.dirname(__file__), "config\\credentials.yaml")
+
+with open(fullpath, 'r') as file:
+    global yaml_config
+    yaml_config = yaml.safe_load(file)
+
+username = yaml_config["defaults"]["snowflake"]["username"]
+password = yaml_config["defaults"]["snowflake"]["password"]
+warehouse = yaml_config["defaults"]["snowflake"]["warehouse"]
+database = yaml_config["defaults"]["snowflake"]["database"]
+schema = yaml_config["defaults"]["snowflake"]["schema"]
+host = yaml_config["defaults"]["snowflake"]["host"]
+account_id = yaml_config["defaults"]["snowflake"]["account_id"]
+
 #-----------------------------------------------------------
 class SnowflakeTask(threading.Thread, SnowflakeConnector):
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
-        #Tasks to be done
-        print("\nStarting AWS threaded execution...\n")
+        print("\nStarting Snowflake threaded execution...\n")
         try:
             # TODO: Insert your code here
             pass
